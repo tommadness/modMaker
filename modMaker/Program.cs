9bf26584f0b477b993756cc5fa05ded726112f88
tommadness <tom.m.cover@gmail.com>
2021-03-21 10:35:33 -0400

    Add project files.

---
+++ modMaker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace modMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles("mod", "*.*", SearchOption.AllDirectories);
+            var output = "assets:\r";
+            foreach (var file in files)
+            {
+                output += String.Format("- name: {0}\r  method: copy\r  source:\r  - name: {0}\r", file.Remove(0,4));
+            }
+            File.WriteAllText("mod/OUTPUTmod.yml", output);
+        }
+    }
+}
